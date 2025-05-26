25ec6c42100a4732af0278277e1b28187e701050
vinaykumar9848 <vinaykumar9848@hotmail.com>
2025-05-26 12:53:28 +0530

    update create.tf via script

--- create.tf
+++ create.tf
@@ -1,16 +1,34 @@
 this is the temporary file
+this is the temporary file
+
 
 /*
+/*
+
 
 */
+*/
 #hello
+#hello
+# daily updates start
+/*
 # daily updates start
 
 
+
+
+hello
 hello
 
 
+
+
+*/
+# daily updates end
 # daily updates end
 /*
+/*
 
+
+*/
 */
